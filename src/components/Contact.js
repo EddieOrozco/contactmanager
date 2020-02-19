237636daac67ce65b3af23759c8f34d99c599555
EddieOrozco <orozcoe230@gmail.com>
2020-02-19 12:43:14 -0600

    Destructuring

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
+    const { name, email, phone } = this.props;
     return (
       <div>
-        <h4>{this.props.name}</h4>
+        <h4>{name}</h4>
         <ul>
-          <li>{this.props.email}</li>
-          <li>{this.props.phone}</li>
+          <li>{email}</li>
+          <li>{phone}</li>
         </ul>
       </div>
     );
