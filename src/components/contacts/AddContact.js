a95875d56254db289f4907363cc99edf8819c1ac
EddieOrozco <orozcoe230@gmail.com>
2020-04-14 13:47:14 -0500

    Created POST & DELETE Request

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
 import TextInputGroup from "../layout/TextInputGroup";
-import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 class AddContact extends Component {
   state = {
@@ -35,13 +35,13 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuidv4(),
       name,
       email,
       phone,
     };
-
-    dispatch({ type: "ADD_CONTACT", payload: newContact });
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newContact)
+      .then((res) => dispatch({ type: "ADD_CONTACT", payload: res.data }));
 
     // Clear State
     this.setState({
